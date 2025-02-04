582fc82de9535c9b8401aaf14e9dc995bab3e0b0
Juno Nguyen <juno.ngx@gmail.com>
2025-02-04 22:24:46 +0800

    Implement useOsServerSide

---
+++ src/utils/useOsServerSide.ts
@@ -0,0 +1,17 @@
+import { headers } from "next/headers";
+
+export async function useOs() {
+    const headersList = await headers();
+    const userAgent = headersList.get("user-agent");
+
+    if (!userAgent) return "unknown";
+
+    if (userAgent.includes("Windows")) return "win";
+    if (userAgent.includes("Mac")) return "mac";
+    if (userAgent.includes("Linux")) return "linux";
+    if (userAgent.includes("Android")) return "android";
+    if (userAgent.includes("iPhone") || userAgent.includes("iPad"))
+        return "ios";
+
+    return "unknown";
+}
