88025b530d04bb0d2c450088131f936717e63f2d
Juno Nguyen <juno.ngx@gmail.com>
2025-02-04 22:18:35 +0800

    Carry over Kbd components

---
+++ src/components/misc/KbdAlt.tsx
@@ -0,0 +1,13 @@
+import { useOs } from "@mantine/hooks";
+
+export default function KbdAlt() {
+    const os = useOs();
+
+    switch(true) {
+    case (os === "macos"):
+    case (os === "ios"):
+        return <kbd>Opt</kbd>;
+    default:
+        return <kbd>Alt</kbd>;
+    }
+}
---
+++ src/components/misc/KbdMod.tsx
@@ -0,0 +1,13 @@
+import { useOs } from "@mantine/hooks";
+
+export default function KbdMod() {
+    const os = useOs();
+
+    switch(true) {
+    case (os === "macos"):
+    case (os === "ios"):
+        return <kbd>Cmd</kbd>;
+    default:
+        return <kbd>Ctrl</kbd>;
+    }
+}
